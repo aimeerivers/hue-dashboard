d631bc22938a89fc6130880dee3b91366f4dc53b
Aimee Rivers <airi@dr.dk>
2021-02-23 20:49:43 +0100

    ALLLLLLL the colours!!!

--- src/server.ts
+++ src/server.ts
@@ -219,8 +219,9 @@ function drawSceneColours(sceneColours) {
   const canvas = createCanvas(144, 144);
   let ctx = canvas.getContext('2d');
   let grd = ctx.createLinearGradient(0, 0, 200, 200);
-  grd.addColorStop(0, sceneColours[0]);
-  // grd.addColorStop(0.5, "green");
+  for(var index = 0; index < sceneColours.length - 1; index++) {
+    grd.addColorStop(index/sceneColours.length, sceneColours[index]);
+  }
   grd.addColorStop(1, sceneColours[sceneColours.length - 1]);
   ctx.fillStyle = grd;
   ctx.fillRect(0, 0, 144, 144);
